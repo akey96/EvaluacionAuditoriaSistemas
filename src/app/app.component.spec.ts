0c26030fd2c48badec8041be049cd9c51eabf5cd
Alexander Mamani <akeymy4@gmail.com>
2020-11-05 08:54:39 -0400

    adding test box black to AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   // it('should create the app', () => {
@@ -28,4 +38,33 @@ describe('AppComponent', () => {
   //   const compiled = fixture.nativeElement;
   //   expect(compiled.querySelector('.content span').textContent).toContain('mediaAritmetica app is running!');
   // });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('mediaAritmetica app is running!');
+  // });
+
+  it('Los campos de rangos de valores se crean con valores vacios', () => {
+    const elementHTML: HTMLElement = fixture.nativeElement;
+    const minimoElement = elementHTML.querySelector('#idMinimo');
+    const maximoElement = elementHTML.querySelector('#idMaximo');
+    expect(minimoElement.innerHTML).toEqual('');
+    expect(maximoElement.innerHTML).toEqual('');
+  });
+
+  it('Los valores de rango de valor minimo tiene que ser menor al campo mayor', () => {
+    const elementHTML: HTMLElement = fixture.nativeElement;
+    const minimoElement = elementHTML.querySelector('#idMinimo');
+    const maximoElement = elementHTML.querySelector('#idMaximo');
+
+    minimoElement.innerHTML = '23'
+    maximoElement.innerHTML = '45'
+
+    expect(minimoElement.classList.contains('is-valid')).toBeTruthy()
+
+  });
+
+
 });
